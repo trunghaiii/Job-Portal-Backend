48a3bb18548e92c95b94349e9667f5c5b63f25c6
trunghaiii <haitran022001@gmail.com>
2023-08-25 21:25:50 -0400

    coding Create new User Api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ResponseMessage } from 'src/decorators/decoratorCustomize';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @ResponseMessage("Create New User Successfully!!")
+  create(@Body() createUserDto: CreateUserDto, @Request() req) {
 
-    return this.usersService.create(createUserDto);
+    return this.usersService.create(createUserDto, req.user);
   }
 
   @Get()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,16 +15,33 @@ export class UsersService {
 
   constructor(@InjectModel(User.name) private UserModel: Model<User>) { }
 
-  create(createUserDto: CreateUserDto) {
-    //console.log("bitch", createUserDto);
+  async create(createUserDto: CreateUserDto, user: any) {
+
+    // 0. check if email already existed in database:
+    const existedEmail = await this.UserModel.findOne({ email: createUserDto.email })
+
+    if (existedEmail) throw new BadRequestException("Email Already Existed");
+    // 1. hash the password using bcrypt
     const hash = hashSync(createUserDto.password, saltRounds);
 
-    const createdUser = new this.UserModel({
+    // 2. save user data to the database:
+    const createUser = new this.UserModel({
       name: createUserDto.name,
       email: createUserDto.email,
-      password: hash
-    });
-    return createdUser.save();
+      password: hash,
+      age: +createUserDto.age,
+      gender: createUserDto.gender,
+      address: createUserDto.address,
+      role: createUserDto.role,
+      company: createUserDto.company,
+      createdBy: {
+        id: user._id,
+        email: user.email
+      }
+    }).save();
+
+    return createUser;
+
 
   }
 
