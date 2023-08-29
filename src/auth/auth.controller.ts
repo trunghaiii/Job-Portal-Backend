af8e30febcff4f19456bb3a3ac89f9602c7b7961
trunghaiii <haitran022001@gmail.com>
2023-08-28 22:44:58 -0400

    coding get account user data Api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Request, UseGuards, Body, Res } from '@nestjs/common';
 import { AppService } from 'src/app.service';
 import { AuthService } from './auth.service';
-import { Public, ResponseMessage } from 'src/decorators/decoratorCustomize';
+import { Public, ResponseMessage, User } from 'src/decorators/decoratorCustomize';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { UsersService } from 'src/users/users.service';
@@ -31,6 +31,14 @@ export class AuthController {
     Register(@Body() registerUserDto: RegisterUserDto) {
 
         return this.usersService.registerUser(registerUserDto);
+    }
+
+
+    @Get("account")
+    @ResponseMessage("Get account user data Successfully!!")
+    Account(@User() user) {
+
+        return { user };
     }
 
     //@UseGuards(JwtAuthGuard)
